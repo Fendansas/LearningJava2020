29d43103a6c783f7373e1b3b162b40911c076d10
Fendansas <53154654+Fendansas@users.noreply.github.com>
2020-06-26 00:35:31 +0300

    V26.06

--- src/com/epam/home/task2/MainPyramid.java
+++ src/com/epam/home/task2/MainPyramid.java
@@ -5,37 +5,24 @@ import java.util.Scanner;
 
 public class MainPyramid {
     public static void main(String[] args) {
-        int[][] arr = new int[5][5];
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                arr[i][j] = j + 1;
-            }
-        }
-        for (int[] anArr : arr) {
-            for (int anAnArr : anArr) {
-                System.out.print(anAnArr + " ");
+
+
+        int max = 4;
+        int min;
+
+        for (int i = 0; i<max; i++){
+            for (int j = 0; j <max; j++){
+                min = i>j?i:j;
+                System.out.print(max-min);
             }
             System.out.println();
+
+        }
+
+
+
+
         }
     }
-}
-  /*  Scanner sc = new Scanner(System.in);
 
-        System.out.println("Here Is Your Pyramid");
-                int noOfRows = sc.nextInt();
-                int rowCount = 1;
-                int[][] mass = new int[noOfRows][noOfRows];
-                for (int i = mass.length/2; i <mass.length; i++) {
-        for (int j = mass[i].length/2; j <mass[i].length; j++){
-        mass[i][j]= rowCount;
-        rowCount++;
-        }
-        }
-        for (int i = 0; i < mass.length; i++) {
-        for (int j = 0; j < mass[i].length; j++) {
-        System.out.print(mass[i][j] + "\t");
-        }
-        System.out.println();
-        }
 
-   */
